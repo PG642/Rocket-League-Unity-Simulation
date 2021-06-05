208824f9b66bf10205d938cc38ff5b504b07c20a
Sebastian <SebastianPrior@outlook.com>
2021-06-05 16:51:43 +0200

    Add scoreboard and count score for each team #14

--- Assets/Scripts/MatchController/MapData.cs
+++ Assets/Scripts/MatchController/MapData.cs
@@ -1,15 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class MapData : MonoBehaviour
 {
     public float diag;
-    public bool isScoredBlue;
-    public bool isScoredRed;
+    public bool isScoredBlue = false;
+    public bool isScoredOrange = false;
+    public int blueScore = 0;
+    public int orangeScore = 0;
 
     private GoalController _blueGoal;
     private GoalController _redGoal;
+
+    private GUIStyle _style;
 
 
     void Start()
@@ -17,19 +23,36 @@ public class MapData : MonoBehaviour
         diag = Vector3.Distance(transform.Find("diagP1").position, transform.Find("diagP2").position);
         _blueGoal = transform.Find("GoalLines").transform.Find("GoalLineBlue").GetComponent<GoalController>();
         _redGoal = transform.Find("GoalLines").transform.Find("GoalLineRed").GetComponent<GoalController>();
+
+        _style = new GUIStyle();
+        _style.normal.textColor = Color.red;
+        _style.fontSize = 25;
+        _style.fontStyle = FontStyle.Bold;
+        _style.alignment = TextAnchor.UpperCenter;
     }
 
-    private void Update()
+    public void NotifyScore(bool isBlueTeam)
     {
-        isScoredBlue = _blueGoal.isScored;
-        isScoredRed = _redGoal.isScored;
-    }   
+        if (isBlueTeam)
+        {
+            isScoredBlue = true;
+            blueScore++;
+        }
+        else
+        {
+            isScoredOrange = true;
+            orangeScore++;
+        }
+    }
 
-    public void Reset()
+    public void ResetIsScored()
     {
         isScoredBlue = false;
-        isScoredRed = false;
-        _blueGoal.isScored = false;
-        _redGoal.isScored = false;
+        isScoredOrange = false;
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width / 2 - 75, 0f, 150, 130), $"{blueScore:D2} : {orangeScore:D2}", _style);
     }
 }
