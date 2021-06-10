f326797756dd5935ef7b543bcfef4d5fb5718e90
Leon Büttinghaus <leon_buettinghaus@web.de>
2021-06-10 16:01:26 +0200

    Added car boost management

--- Assets/Scripts/CarControllers/CubeController/CubeBoosting.cs
+++ Assets/Scripts/CarControllers/CubeController/CubeBoosting.cs
@@ -5,17 +5,21 @@ public class CubeBoosting : MonoBehaviour
 {
     public float BoostForceMultiplier = 1f;
     const float BoostForce = 991 / 100;
-    
+
+    public bool isBoosting = false;
+
+    private float _boostAmount = 32f;
+    private int _boostCountdown = 13;
     CubeController _c;
     InputManager _inputManager;
     Rigidbody _rb;
-    
+
     private void Start()
     {
         _inputManager = GetComponentInParent<InputManager>();
         _c = GetComponent<CubeController>();
         _rb = GetComponentInParent<Rigidbody>();
-        
+
         // Activate ParticleSystems GameObject
         if (Resources.FindObjectsOfTypeAll<CubeParticleSystem>()[0] != null)
             Resources.FindObjectsOfTypeAll<CubeParticleSystem>()[0].gameObject.SetActive(true);
@@ -25,12 +29,33 @@ public class CubeBoosting : MonoBehaviour
     {
         Boosting();
     }
-    
+
     void Boosting()
     {
-        if (_inputManager.isBoost && _c.forwardSpeed < CubeController.MaxSpeedBoost)
+        if (_inputManager.isBoost || (_boostCountdown < 13 && _boostCountdown > 0))
         {
-                _rb.AddForce(BoostForce * BoostForceMultiplier * transform.forward, ForceMode.Acceleration);
+            _boostCountdown--;
+            if (_boostAmount > 0)
+            {
+                isBoosting = true;
+                if (_c.forwardSpeed < CubeController.MaxSpeedBoost)
+                {
+                    _rb.AddForce(BoostForce * BoostForceMultiplier * transform.forward, ForceMode.Acceleration);
+                }
+                _boostAmount = Mathf.Max(0.0f, _boostAmount - 0.27f);
+
+            }
+            else
+            {
+                isBoosting = false;
+            }
         }
+        else
+        {
+            _boostCountdown = 13;
+            isBoosting = false;
+        }
+        Debug.Log($"Boost: {_boostAmount}");
+
     }
 }
