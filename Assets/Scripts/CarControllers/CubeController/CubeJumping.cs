044cd87f296a0f3fe24895e34579ba6dcee45f83
Jannik Drögemüller <jannik.droegemueller@tu-dortmund.de>
2021-06-15 17:50:01 +0200

    reimplement jumping logic including second jump and vertical momentum for dodges

--- Assets/Scripts/CarControllers/CubeController/CubeJumping.cs
+++ Assets/Scripts/CarControllers/CubeController/CubeJumping.cs
@@ -3,19 +3,21 @@
 [RequireComponent(typeof(CubeController))]
 public class CubeJumping : MonoBehaviour
 {
-    [Header("Forces")]
-    [Range(0.25f,4)]
-    // default 1
-    public float jumpForceMultiplier = 1f;
-    public int upForce = 3;
-    public int upTorque = 50;
-    
-    float _jumpTimer = 0;
-    [SerializeField]
-    bool _isCanFirstJump = false;
-    bool _isJumping = false;
-    [SerializeField]
-    bool _isCanKeepJumping = false;
+
+    float _timerJumpButtonHeld = 0;
+    float _timerSecondJump = 0;
+
+    bool _isFirstJumpPress = false;
+    bool _isFirstJump = false;
+    bool _isSecondJump = false;
+    bool _isSecondJumpUsed = false;
+    bool _lowerSecondJumpTimer = false;
+    bool _lastFrameNoButton = true;
+
+    bool _isDodge = false;
+    float _timerDodge = 0f;
+    const float _dodgeDeadzone = 0.50f;
+
 
     Rigidbody _rb;
     InputManager _inputManager;
@@ -30,13 +32,106 @@ public class CubeJumping : MonoBehaviour
 
     private void FixedUpdate()
     {
+        UpdateJumpVariables();
         Jump();
-        JumpBackToTheFeet();
+        //JumpBackToTheFeet();
+    }
+
+    private void UpdateJumpVariables()
+    {
+        bool carIsGrounded = _controller.carState == CubeController.CarStates.AllWheelsSurface || _controller.carState == CubeController.CarStates.AllWheelsGround;
+        if(carIsGrounded)
+        {
+            _isSecondJumpUsed = false;
+            _lowerSecondJumpTimer = false;
+            _timerSecondJump = 1.25f;
+        }
+
+        if (_lowerSecondJumpTimer)
+        {
+            _timerSecondJump -= Time.deltaTime;
+        }
+
+
+        if(_isFirstJump)
+        {
+            _timerJumpButtonHeld -= Time.deltaTime;
+        }
+        
+        if(_isFirstJump && (_timerJumpButtonHeld <= 0f || !_inputManager.isJump))
+        {
+            _isFirstJump = false;
+            _lowerSecondJumpTimer = true;
+            _timerSecondJump = 1.25f;
+        }
+
+        if (!_isFirstJump && carIsGrounded && _inputManager.isJump && _lastFrameNoButton)
+        {
+            _isFirstJumpPress = true;
+            _isFirstJump = true;
+            _timerJumpButtonHeld = 0.2f;
+        }
+
+        if(!_isFirstJump && !carIsGrounded && _inputManager.isJump && !_isSecondJumpUsed && _timerSecondJump > 0f && _lastFrameNoButton)
+        {
+            _isSecondJump = true;
+            if(Mathf.Abs(_inputManager.yawInput) > _dodgeDeadzone || Mathf.Abs(_inputManager.pitchInput) > _dodgeDeadzone)
+            {
+                _isDodge = true;
+                _timerDodge = 0f;
+            }
+        }
+
+        _lastFrameNoButton = !_inputManager.isJump;
     }
 
     private void Jump()
     {
-        // Do initial jump impulse only once
+        //First Jump
+        if(_isFirstJump)
+        {
+            if(_isFirstJumpPress)
+            {
+                _isFirstJumpPress = false;
+                _rb.AddForce(transform.up * 2.92f, ForceMode.VelocityChange);
+            }
+            else
+            {
+                _rb.AddForce(transform.up * 14.58f, ForceMode.Acceleration);
+            }
+        }
+
+        //Second Jump
+        if(_isSecondJump)
+        {
+            _isSecondJumpUsed = true;
+            if (_isDodge)
+            {
+                if(_timerDodge >= 0.15f && _timerDodge <= 0.65f )
+                {
+                    _rb.velocity = Vector3.Scale(_rb.velocity, new Vector3(1f, 0.65f, 1f));
+                    Debug.Log("Dodge detected, timer = " + _timerDodge);
+                }
+                else if(_timerDodge > 0.65f)
+                {
+                    _isDodge = false;
+                    _isSecondJump = false;
+                }
+                _timerDodge += Time.deltaTime;
+            }
+            else
+            { 
+                _isSecondJump = false;
+                _rb.AddForce(transform.up * 2.92f, ForceMode.VelocityChange);
+            }
+        }
+
+
+
+
+
+    }
+        /*// Do initial jump impulse only once
         // TODO: Currently bugged, should be .isJumpDown for the initial jump impulse.
         // Right now does the whole jump impulse
         if (_inputManager.isJump && _isCanFirstJump)
@@ -85,5 +180,5 @@ public class CubeJumping : MonoBehaviour
             _rb.AddForce(Vector3.up * upForce, ForceMode.VelocityChange);
             _rb.AddTorque(transform.forward * upTorque, ForceMode.VelocityChange);
         }
-    }
+    }*/
 }
