cc7167c182f4904889506c70672c3431c7feef3b
Thilo Röthemeyer <26631624+TheImus@users.noreply.github.com>
2021-06-21 17:47:00 +0200

    Added Startoffset of car

--- Assets/TestController.cs
+++ Assets/TestController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Json;
-using Tests;
+using JsonObjects;
 using UnityEngine;
 
 public class TestController : MonoBehaviour
@@ -13,21 +13,20 @@ public class TestController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var fromJson = JsonUtility.FromJson<Root>(jsonFile.text);
+        var fromJson = JsonUtility.FromJson<RootJson>(jsonFile.text);
         Debug.Log(fromJson);
         SetupCar(fromJson);
     }
 
-    private void SetupCar(Root fromJson)
+    private void SetupCar(RootJson fromJson)
     {
         var car = transform.Find("ControllableCar");
 
         var carStartValue = fromJson.startValues.Find(x => x.name == "car");
-        var rotation = Quaternion.Euler(carStartValue.rotation.x, carStartValue.rotation.y, carStartValue.rotation.z);
-        var position = new Vector3(carStartValue.position.x, carStartValue.position.y, carStartValue.position.z);
-        //car.SetPositionAndRotation(position,rotation);
+        var rotation = Quaternion.Euler(carStartValue.rotation.x + 0.55f, carStartValue.rotation.y, carStartValue.rotation.z);
+        var position = new Vector3(carStartValue.position.x, carStartValue.position.y +0.1701f, carStartValue.position.z);
         var car_rb =GetComponentsInChildren<Rigidbody>().Where(x => x.tag == "ControllableCar").FirstOrDefault();
-        car_rb.position = position + new Vector3(0.0f,0.18f,0.0f);
+        car_rb.position = position;
         car_rb.rotation = rotation;
         car_rb.velocity = new Vector3(carStartValue.velocity.x, carStartValue.velocity.y, carStartValue.velocity.z);
         var result = StartCoroutine(TestMethod(3, car_rb, carStartValue));
