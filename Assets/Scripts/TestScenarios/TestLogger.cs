062faf18173e35ba48649f2333d2978171279a4d
Hendrik Meyer <34685669+hndm@users.noreply.github.com>
2021-07-05 18:39:29 +0200

    Iss48 Create Test Controller for testing the physic (#58)
    * Added json example for Setup Simulation
    
    * added TestController
    
    * added more properties
    
    * fast Commit
    
    * Added Startoffset of car
    
    * Refactoring
    
    * Update Test.unity
    
    * Inputs im Testcontroller an Inputcontroller weitergebenen
    
    * added ball start inputs and refactoring
    
    * refactoring and bug fixing
    
    * Refactoring and logging runs till time of scenario
    
    * fixed null exception
    
    * moved classes to directories
    
    * moved TestCases into Namespaces
    
    * changed log values and names
    
    * add gameinformationController
    
    * roll pitch yaw
    
    * changed log input and output names
    
    * added compatibility to Plotter
    
    * added settings.json for paths
    
    * changed settings.json
    
    * added settings.json for paths
    
    changed settings.json
    
    * metadata
    
    * Update .gitignore
    
    added settings to git ignore
    
    * test gitignore
    
    * test
    
    * test
    
    * Create settings.json
    
    * Update settings.json
    
    * start at 0.0
    
    * fixed time bug
    
    * Fix settings file imports and add angular velocity logging
    
    * Politur
    
    * d
    
    * delete setting for fresh ignore
    
    * sdf
    
    Co-authored-by: Thilo Röthemeyer <26631624+TheImus@users.noreply.github.com>
    Co-authored-by: Kabumba <Freddy253@live.de>

---
+++ Assets/Scripts/TestScenarios/TestLogger.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Extentions;
+using TestScenarios.JsonClasses;
+using UnityEngine;
+
+namespace TestScenarios
+{
+    public class TestLogger
+    {
+        private Rigidbody _rigidbodyCar;
+        private readonly InputManager _inputManager;
+        private readonly string _loggingPath;
+        private Log _currentLog;
+        private readonly Rigidbody _rigidbodyBall;
+        
+
+        public TestLogger(Rigidbody rigidbodyCar, Rigidbody rigidbodyBall, JsonClasses.Scenario scenario,
+            InputManager inputManager, string loggingPath)
+        {
+            _rigidbodyCar = rigidbodyCar;
+            _rigidbodyBall = rigidbodyBall;
+            _inputManager = inputManager;
+            _loggingPath = loggingPath;
+
+            StartNewLogging(scenario.name);
+        }
+
+        public void StartNewLogging(string scenarioName)
+        {
+            _currentLog = new Log()
+            {
+                name = scenarioName,
+                frames = new List<LogValue>()
+            };
+        }
+
+        public void Log(float boost, bool wheelsOnGround, bool jumped)
+        {
+            var logValue = new LogValue()
+            {
+                time = Time.time,
+                game_cars = new List<GameCar>()
+                {
+                    new GameCar()
+                    {
+                        physics = new GameObjectSaveValue()
+                        {
+                            location = _rigidbodyCar.position.ToVector(),
+                            velocity = _rigidbodyCar.velocity.ToVector(),
+                            rotation = _rigidbodyCar.rotation.ToVector(),
+                            angular_velocity = _rigidbodyCar.angularVelocity.ToVector()
+                        },
+
+                        boost = boost,
+                        has_wheel_contact = wheelsOnGround,
+                        jumped = jumped,
+                    }
+                },
+                game_ball = new BallValues()
+                {
+                    physics = new GameObjectSaveValue()
+                    {
+                        location = _rigidbodyBall.position.ToVector(),
+                        velocity = _rigidbodyBall.velocity.ToVector(),
+                        rotation = _rigidbodyBall.rotation.ToVector(),
+                        angular_velocity = _rigidbodyCar.angularVelocity.ToVector()
+                    }
+                }
+            };
+            _currentLog.frames.Add(logValue);
+        }
+
+        public void SaveLog()
+        {
+            SaveAsJson(_currentLog);
+        }
+
+        private void SaveAsJson(Log log)
+        {
+            string json = JsonUtility.ToJson(log);
+            string path = _loggingPath + $"{log.name}.json";
+            using (StreamWriter file = new StreamWriter(path))
+            {
+                string msg = JsonUtility.ToJson(log, true);
+                file.Write(msg);
+                Debug.Log($"Saved{log.name} at {path}");
+            }
+        }
+    }
+}
