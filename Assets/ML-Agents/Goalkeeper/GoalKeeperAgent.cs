92a97aceedc36c1a9f7eccadcae849d9a91acac9
Konstantin Ramthun <konstantin_ramthun@web.de>
2021-11-15 11:05:50 +0100

    Added different action handling methods, depending on the action specifications of the behavior parameters in unity.

--- Assets/ML-Agents/Goalkeeper/GoalKeeperAgent.cs
+++ Assets/ML-Agents/Goalkeeper/GoalKeeperAgent.cs
@@ -1,9 +1,12 @@
 using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Policies;
+
 
 public class GoalKeeperAgent : Agent
 {
@@ -27,10 +30,24 @@ public class GoalKeeperAgent : Agent
     private MapData _mapData;
 
     public InputManager InputManager;
+
+    /// <summary>
+    /// Contains the possible values for the discretized actions.
+    /// </summary>
+    private readonly float[] DISCRETE_ACTIONS = { -1f, -0.5f, 0f, 0.5f, 1f };
+
+    /// <summary>
+    /// Shows whether the action space of the agent is continuous, multi-discrete or mixed.
+    /// </summary>
+    private ActionSpaceType actionSpaceType;
+    
     void Start()
     {
         InputManager = GetComponent<InputManager>();
         InputManager.isAgent = true;
+
+        ActionSpec actionSpec = GetComponent<BehaviorParameters>().BrainParameters.ActionSpec;
+        actionSpaceType = DetermineActionSpaceType(actionSpec);
 
         _rb = GetComponent<Rigidbody>();
         _airControl = GetComponentInChildren<CubeAirControl>();
@@ -103,20 +120,21 @@ public class GoalKeeperAgent : Agent
     {
         if (InputManager.isAgent)
         {
-            // set inputs
-            InputManager.throttleInput = actionBuffers.ContinuousActions[0];
-            InputManager.steerInput = actionBuffers.ContinuousActions[1];
-            InputManager.yawInput = actionBuffers.ContinuousActions[1];
-            InputManager.pitchInput = actionBuffers.ContinuousActions[2];
-            InputManager.rollInput = 0;
-            if (actionBuffers.ContinuousActions[3] > 0) InputManager.rollInput = 1;
-            if (actionBuffers.ContinuousActions[3] < 0) InputManager.rollInput = -1;
-
-            InputManager.isBoost = actionBuffers.ContinuousActions[4] > 0;
-            InputManager.isDrift = actionBuffers.ContinuousActions[5] > 0;
-            InputManager.isAirRoll = actionBuffers.ContinuousActions[6] > 0;
-
-            InputManager.isJump = actionBuffers.ContinuousActions[7] > 0;
+            switch (actionSpaceType)
+            {
+                case ActionSpaceType.Continuous:
+                    ProcessContinuousActions(actionBuffers);
+                    break;
+                case ActionSpaceType.MultiDiscrete:
+                    ProcessMultiDiscreteActions(actionBuffers);
+                    break;
+                case ActionSpaceType.Mixed:
+                    ProcessMixedActions(actionBuffers);
+                    break;
+                default:
+                    throw new InvalidOperationException(string.Format("The method {0} does not support the {1} '{2}'.", nameof(OnActionReceived), typeof(ActionSpaceType), actionSpaceType.ToString()));
+                    break;
+            }
         }
         AssignReward();
     }
@@ -155,5 +173,159 @@ public class GoalKeeperAgent : Agent
             SetReward(-1f);
             Reset();
         }
+    }
+
+    /// <summary>
+    /// Processes the actions, if <see cref="actionSpaceType"/> is <see cref="ActionSpaceType.Continuous"/>.
+    /// </summary>
+    /// <param name="actionBuffers">The action buffers containing the actions.</param>
+    private void ProcessContinuousActions(ActionBuffers actionBuffers)
+    {
+            // set inputs
+            InputManager.throttleInput = actionBuffers.ContinuousActions[0];
+            InputManager.steerInput = actionBuffers.ContinuousActions[1];
+            InputManager.yawInput = actionBuffers.ContinuousActions[1];
+            InputManager.pitchInput = actionBuffers.ContinuousActions[2];
+            InputManager.rollInput = 0;
+            if (actionBuffers.ContinuousActions[3] > 0) InputManager.rollInput = 1;
+            if (actionBuffers.ContinuousActions[3] < 0) InputManager.rollInput = -1;
+
+            InputManager.isBoost = actionBuffers.ContinuousActions[4] > 0;
+            InputManager.isDrift = actionBuffers.ContinuousActions[5] > 0;
+            InputManager.isAirRoll = actionBuffers.ContinuousActions[6] > 0;
+
+            InputManager.isJump = actionBuffers.ContinuousActions[7] > 0;
+    }
+
+    /// <summary>
+    /// Processes the actions, if <see cref="actionSpaceType"/> is <see cref="ActionSpaceType.MultiDiscrete"/>.
+    /// </summary>
+    /// <param name="actionBuffers">The action buffers containing the actions.</param>
+    private void ProcessMultiDiscreteActions(ActionBuffers actionBuffers)
+    {
+        InputManager.throttleInput = DISCRETE_ACTIONS[actionBuffers.DiscreteActions[0]];
+        InputManager.steerInput = DISCRETE_ACTIONS[actionBuffers.DiscreteActions[1]];
+        InputManager.yawInput = DISCRETE_ACTIONS[actionBuffers.DiscreteActions[1]];
+        InputManager.pitchInput = DISCRETE_ACTIONS[actionBuffers.DiscreteActions[2]];
+        switch (actionBuffers.DiscreteActions[3])
+        {
+            case 0:
+                InputManager.rollInput = -1;
+                break;
+            case 2:
+                InputManager.rollInput = 1;
+                break;
+            default:
+                InputManager.rollInput = 0;
+                break;
+        }
+        InputManager.isBoost = actionBuffers.DiscreteActions[4] > 0;
+        InputManager.isDrift = actionBuffers.DiscreteActions[5] > 0;
+        InputManager.isAirRoll = actionBuffers.DiscreteActions[6] > 0;
+        InputManager.isJump = actionBuffers.DiscreteActions[7] > 0;
+    }
+
+    /// <summary>
+    /// Processes the actions, if <see cref="actionSpaceType"/> is <see cref="ActionSpaceType.Mixed"/>.
+    /// </summary>
+    /// <param name="actionBuffers">The action buffers containing the actions.</param>
+    private void ProcessMixedActions(ActionBuffers actionBuffers)
+    {
+        //Continuous actions
+        InputManager.throttleInput = actionBuffers.ContinuousActions[0];
+        InputManager.steerInput = actionBuffers.ContinuousActions[1];
+        InputManager.yawInput = actionBuffers.ContinuousActions[1];
+        InputManager.pitchInput = actionBuffers.ContinuousActions[2];
+
+        //Discrete actions
+        switch (actionBuffers.DiscreteActions[0])
+        {
+            case 0:
+                InputManager.rollInput = -1;
+                break;
+            case 2:
+                InputManager.rollInput = 1;
+                break;
+            default:
+                InputManager.rollInput = 0;
+                break;
+        }
+        InputManager.isBoost = actionBuffers.DiscreteActions[1] > 0;
+        InputManager.isDrift = actionBuffers.DiscreteActions[2] > 0;
+        InputManager.isAirRoll = actionBuffers.DiscreteActions[3] > 0;
+        InputManager.isJump = actionBuffers.DiscreteActions[4] > 0;
+    }
+
+    /// <summary>
+    /// Determines the action space type for the specified <paramref name="actionSpec"/>.
+    /// </summary>
+    /// <param name="actionSpec">The <see cref="ActionSpec"/> for which we want to determine the action sapce type.</param>
+    /// <returns>The corresponding action space type.</returns>
+    /// <exception cref="ArgumentNullException">If <paramref name="actionSpec"/> is <c>null</c>.</exception>
+    private static ActionSpaceType DetermineActionSpaceType(ActionSpec actionSpec)
+    {
+        // Check for invalid parameter
+        _ == actionSpec ?? throw new ArgumentNullException(nameof(actionSpec));
+
+        int requiredNumActions = 8;
+
+        // Determine action space type
+        if(actionSpec.NumContinuousActions > 0 && actionSpec.NumDiscreteActions == 0)
+        {
+            //Propably continuous, we check the size
+            if(actionSpec.NumContinuousActions != requiredNumActions)
+            {
+                throw new ArgumentException(string.Format("It seems like you tried to use a continuos action space for the agent. In this case the {0} needs 8 continuous actions.", typeof(GoalKeeperAgent)));
+            }
+            return ActionSpaceType.Continuous;
+        }
+        else if(actionSpec.NumContinuousActions == 0 && actionSpec.NumDiscreteActions > 0)
+        {
+            //Propably multi discrete, we check the size
+            if (actionSpec.NumDiscreteActions != requiredNumActions)
+            {
+                throw new ArgumentException(string.Format("It seems like you tried to use a multi-discrete action space for the agent. In this case the {0} needs 8 discrete action branches.", typeof(GoalKeeperAgent)));
+            }
+            int[] requiredBranchSizes = { DISCRETE_ACTIONS.Length, DISCRETE_ACTIONS.Length, DISCRETE_ACTIONS.Length, 3, 2, 2, 2, 2};
+            for(int i = 0; i < actionSpec.BranchSizes; i++)
+            {
+                if(actionSpec.BranchSizes[i] != requiredBranchSizes[i])
+                {
+                    throw new ArgumentException(string.Format("It seems like you tried to use a multi-discrete action space for the agent. In this case the {0} needs 8 discrete action branches with sizes ({1}).", 
+                        typeof(GoalKeeperAgent), 
+                        string.Join(", ", requiredBranchSizes)));
+                }
+            }
+            return ActionSpaceType.MultiDiscrete;
+        }
+        else
+        {
+            //Propably multi discrete, we check the size
+            if (actionSpec.NumDiscreteActions != 5 || actionSpec.NumContinuousActions != 3)
+            {
+                throw new ArgumentException(string.Format("It seems like you tried to use a mixed action space for the agent. In this case the {0} needs 5 discrete action branches and 3 continuous actions.", typeof(GoalKeeperAgent)));
+            }
+            int[] requiredBranchSizes = { 3, 2, 2, 2, 2 };
+            for (int i = 0; i < actionSpec.BranchSizes; i++)
+            {
+                if (actionSpec.BranchSizes[i] != requiredBranchSizes[i])
+                {
+                    throw new ArgumentException(string.Format("It seems like you tried to use a mixed action space for the agent. In this case the {0} needs 5 discrete action branches with sizes ({1}).",
+                        typeof(GoalKeeperAgent),
+                        string.Join(", ", requiredBranchSizes)));
+                }
+            }
+            return ActionSpaceType.Mixed;
+        }
+    }
+
+    /// <summary>
+    /// Helper for classifying the action space.
+    /// </summary>
+    private enum ActionSpaceType
+    {
+        Continuous,
+        MultiDiscrete,
+        Mixed
     }
 }
